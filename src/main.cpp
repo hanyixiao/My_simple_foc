e18a016edd1b1d7bce2e823f3a5a1578b590dd7a
bugs_maker <2456181875@qq.com>
2023-07-25 22:07:14 +0800

    as5600 encoder test success

--- src/main.cpp
+++ src/main.cpp
@@ -19,15 +19,15 @@ void setup()
   I2Ctwo.begin(23, 5, 400000UL);
 
   sensor0.init(&I2Cone);
-  sensor1.init(&I2Ctwo);
-  do{
-    mpu6050.begin();
-  }while(!mpu6050.isReady());
-  
-  Serial.print("mpu6050 init success");
+  // sensor1.init(&I2Ctwo);
+  // do{
+  //   mpu6050.begin();
+  // }while(!mpu6050.isReady());
+
+  // Serial.print("mpu6050 init success");
   Serial.print('\t');
   
-  mpu6050.calcGyroOffsets(true);
+  // mpu6050.calcGyroOffsets(true);
   pinMode(32, OUTPUT); // 将数字引脚2设置为输出模式 
   pinMode(33, OUTPUT); 
   pinMode(25, OUTPUT); 
@@ -45,33 +45,33 @@ void setup()
 
 void loop()
 {
-  mpu6050.update();
+  // mpu6050.update();
   // 角度读取
-  double mpu_yaw = mpu6050.getAngleZ();   // tockn的getangle，通过一阶置信计算
-  double mpu_pitch = mpu6050.getAngleY(); // tockn的getangle，通过一阶置信计算
-  double mpu_roll = mpu6050.getAngleX();  // tockn的getangle，通过一阶置信计算
-  // 加速度读取
-  //    double accX = mpu6050.getAccX();
-  //    double accY = mpu6050.getAccY();
-  //    double accZ = mpu6050.getAccZ();
-  Serial.print("mpu_roll:");
-  Serial.print(mpu_roll);
-  Serial.print('\t');
-  Serial.print("mpu_pitch:");
-  Serial.print(mpu_pitch);
-  Serial.print('\t');
-  Serial.print("mpu_yaw:");
-  Serial.println(mpu_yaw);
-  Serial.print('\t');
+  // double mpu_yaw = mpu6050.getAngleZ();   // tockn的getangle，通过一阶置信计算
+  // double mpu_pitch = mpu6050.getAngleY(); // tockn的getangle，通过一阶置信计算
+  // double mpu_roll = mpu6050.getAngleX();  // tockn的getangle，通过一阶置信计算
+  // // 加速度读取
+  // //    double accX = mpu6050.getAccX();
+  // //    double accY = mpu6050.getAccY();
+  // //    double accZ = mpu6050.getAccZ();
+  // Serial.print("mpu_roll:");
+  // Serial.print(mpu_roll);
+  // Serial.print('\t');
+  // Serial.print("mpu_pitch:");
+  // Serial.print(mpu_pitch);
+  // Serial.print('\t');
+  // Serial.print("mpu_yaw:");
+  // Serial.println(mpu_yaw);
+  // Serial.print('\t');
   //_delay(200);
-  // sensor0.update();
+  sensor0.update();
   // sensor1.update();
 
-  // Serial.print("sensor0:");
-  // Serial.print(sensor0.getAngle());
-  // Serial.print('\t');
+  Serial.print("sensor0:");
+  Serial.print(sensor0.getAngle());
+  Serial.print('\t');
   // Serial.print("sensor1:");
   // Serial.print(sensor1.getAngle());
   Serial.print('\t');
-  delay(1000);
+  delay(100);
 }
