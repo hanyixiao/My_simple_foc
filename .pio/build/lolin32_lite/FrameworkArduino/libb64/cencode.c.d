76fbf8ccdc43161038b5029c425666c61d6041dc
bugs_maker <2456181875@qq.com>
2023-08-15 23:36:38 +0800

    add simplefoc lib

---
+++ .pio/build/lolin32_lite/FrameworkArduino/libb64/cencode.c.d
@@ -0,0 +1,3 @@
+.pio/build/lolin32_lite/FrameworkArduino/libb64/cencode.c.o: \
+ C:/Users/han24/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
+ C:/Users/han24/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
