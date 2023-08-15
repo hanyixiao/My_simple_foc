6b0186ca7109b67bdaa69dd585b4b16b2dd6296c
bugs_maker <2456181875@qq.com>
2023-08-15 23:36:38 +0800

    add simplefoc lib

---
+++ .pio/build/lolin32_lite/FrameworkArduino/libb64/cdecode.c.d
@@ -0,0 +1,3 @@
+.pio/build/lolin32_lite/FrameworkArduino/libb64/cdecode.c.o: \
+ C:/Users/han24/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
+ C:/Users/han24/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
